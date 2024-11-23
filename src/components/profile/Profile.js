7a0f73132a7a7c35f497b0e2b9bbd5301d43209f
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2024-11-23 09:28:15 +0530

    update

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -14,7 +14,24 @@ const Profile = (props) => {
                             ID: {props.stu.studentId}
                         </td>
                     </tr>
-                    
+                    <tr>
+                        <td>Full Name: {props.stu.firstName + " " + props.stu.lastName}</td>
+                    </tr>
+                    <tr>
+                        <td>Age: {props.stu.age}</td>
+                    </tr>
+                    <tr>
+                        <td>Course: {props.stu.course}</td>
+                    </tr>
+                    <tr>
+                        <td>Gender: {props.stu.gender}</td>
+                    </tr>
+                    <tr>
+                        <td>Address: {props.stu.address.city}</td>
+                    </tr>
+                    <tr>
+                        <td>Skills: {props.stu.skills.map(skill => skill + ", ")}</td>
+                    </tr>
                 </tbody>
             </table>
         </>
